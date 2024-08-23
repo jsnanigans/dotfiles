9b907282c4901a1a0788cda96a5e57ae461c8271
Brendan Mullins <brendan.mullins@9am.health>
2024-08-23 12:01:27 +0200

    change theme to catppuccin

--- nvim/bvim/lua/plugins/colorscheme.lua
+++ nvim/bvim/lua/plugins/colorscheme.lua
@@ -1,34 +1,94 @@
 return {
   {
-    'folke/tokyonight.nvim',
-    lazy = false,
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
-    -- opts = {
-    -- },
-    -- init = function()
-    --   -- TODO: set colorscheme based on system/terminal dark/light mode
-    --   -- Also see utils/colorscheme.lua
-    -- end,
     config = function()
-      require('tokyonight').setup {
-        -- transparent = true,
-        styles = {
-          comments = { italic = true },
-          keywords = {
-            italic = true,
-          },
-          functions = {
-            bold = true,
-          },
-          variables = {
-            bold = true,
-          },
+      require('catppuccin').setup {
+        flavour = 'auto', -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = 'latte',
+          dark = 'mocha',
         },
-        -- style = 'night',
+        transparent_background = false, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = 'dark',
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { 'italic' }, -- Change the style of comments
+          conditionals = { 'italic' },
+          loops = {},
+          functions = { 'bold' },
+          keywords = {
+            'italic',
+          },
+          strings = {},
+          variables = { 'bold' },
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
       }
-      vim.cmd.colorscheme 'tokyonight'
+
+      -- setup must be called before loading
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   -- opts = {
+  --   -- },
+  --   -- init = function()
+  --   --   -- TODO: set colorscheme based on system/terminal dark/light mode
+  --   --   -- Also see utils/colorscheme.lua
+  --   -- end,
+  --   config = function()
+  --     require('tokyonight').setup {
+  --       -- transparent = true,
+  --       styles = {
+  --         comments = { italic = true },
+  --         keywords = {
+  --           italic = true,
+  --         },
+  --         functions = {
+  --           bold = true,
+  --         },
+  --         variables = {
+  --           bold = true,
+  --         },
+  --       },
+  --       -- style = 'night',
+  --     }
+  --     vim.cmd.colorscheme 'tokyonight'
+  --   end,
+  -- },
   --
   -- {
   --   "catppuccin/nvim",
