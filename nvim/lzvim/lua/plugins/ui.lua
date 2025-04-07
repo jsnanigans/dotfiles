bb0566a59f9db475d8f610e4c576e6b8cad125c1
jsnanigans <megamaaaan@gmail.com>
2025-04-07 13:19:14 +0200

    Update lualine configuration to enable lazy loading on event trigger

--- nvim/lzvim/lua/plugins/ui.lua
+++ nvim/lzvim/lua/plugins/ui.lua
@@ -72,8 +72,7 @@ return {
     -- lualine
     {
         "nvim-lualine/lualine.nvim",
-        enabled = false,
-        lazy = false,
+        event = "VeryLazy",
         opts = function(_, opts)
             ---@type table<string, {updated:number, total:number, enabled: boolean, status:string[]}>
             local mutagen = {}
